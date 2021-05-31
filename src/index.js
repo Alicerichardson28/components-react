5b57907950014d92056535994805dad8dd71cd0a
Parada <paradafaii28@gmail.com>
2021-05-30 20:05:10 -0400

    create reusable component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments ">
-            <CommentDetail 
-                author= "Sam" 
-                timeAgo="Today at 4:45 PM" 
-                comment="Very interested blog" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author= "Alex" 
-                timeAgo="Today at 2:00 AM" 
-                comment="Keep up a good work" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author= "Jane" 
-                timeAgo="Yesterday at 5:00 PM" 
-                comment="Nice blog post!" 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author= "Sam" 
+                    timeAgo="Today at 4:45 PM" 
+                    comment="Very interested blog" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author= "Alex" 
+                    timeAgo="Today at 2:00 AM" 
+                    comment="Keep up a good work" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author= "Jane" 
+                    timeAgo="Yesterday at 5:00 PM" 
+                    comment="Nice blog post!" 
+                    avatar={faker.image.avatar()}
+                />
+             </ApprovalCard>
         </div>
     );
         
